71ce61d91fdf4a2dcbe06603e9cb248ea59b8bb5
shihaotan91 <shihaotan91@gmail.com>
2019-07-16 09:14:28 +0800

    WIP - write model specs for monthly point

---
+++ spec/model/monthly_point_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+describe MonthlyPoint do
+  describe "associations" do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe "instance methods" do
+    let(:monthly_point) { create :monthly_point, points: 100}
+    let(:user) { monthly_point.user }
+
+    describe "update points" do
+      it "updates the points" do
+        monthly_point.update_points(500)
+        expect(monthly_point.points).to eq 600
+      end
+    end
+
+    describe "upgrade_user_loyalty_tier" do
+      let(:platinum) { create :loyalty_tier, :platinum }
+
+      before do
+        allow(user).to receive(:upgrade_loyalty_tier)
+      end
+
+      describe "when user has less than 1000 points" do
+        it "does not invoke user upgrade_loyalty_tier method" do
+          monthly_point.points = 500
+          monthly_point.save
+
+          expect(user).not_to have_received(:upgrade_loyalty_tier)
+        end
+      end
+
+      describe "when user has more than 1000points but already has platinum loyalty tier" do
+        it "does not invoke user upgrade_loyalty_tier method" do
+          user.loyalty_tier = platinum
+          user.save
+
+          monthly_point.points = 1500
+          monthly_point.save
+
+          expect(user).not_to have_received(:upgrade_loyalty_tier)
+        end
+      end
+
+      describe "when user has more than 1000points and isn't a platinum member" do
+        it "invokes user upgrade_loyalty_tier method" do
+          monthly_point.points = 1500
+          monthly_point.save
+
+          expect(user).to have_received(:upgrade_loyalty_tier)
+        end
+      end
+    end
+  end
+end
